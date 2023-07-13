03f0650543af14ca08949ce110026662314a0bed
Zsolt Nagy <ghost990@gmail.com>
2023-07-13 18:58:26 +0200

    add: body fixes add: global style

---
+++ src/components/Gift/Gift.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const GiftWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+    height: 100vh;
+`
+
+const Gift = () => {
+  return (
+    <GiftWrapper>Gift</GiftWrapper>
+  )
+}
+
+export default Gift
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,15 @@
 import * as React from "react"
+import Gift from "../components/Gift/Gift"
+import GlobalStyle from "../styles/globalStyles"
 
 
 
 const IndexPage = () => {
   return (
-    <main>
-     main
-    </main>
+    <>
+      <GlobalStyle />
+     <Gift />
+    </>
   )
 }
 
