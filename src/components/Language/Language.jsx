ad4d216046b53a379d1e122eb9ecbf38fdff8d8b
Zsolt Nagy <ghost990@gmail.com>
2023-07-19 18:10:38 +0200

    add: ability to change language

---
+++ src/components/Language/Language.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import propTypes from 'prop-types'
+import styled from 'styled-components'
+import FlagHu from '../../assets/hungary.png'
+import FlagSrb from '../../assets/serbia.png'
+
+const LanguageWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    margin: 15px 0;
+    position: relative;
+    z-index: 1;
+`
+
+const FlagWrapper = styled.div`
+    
+`
+
+const Text = styled.div`
+    margin-left: 10px;
+`
+
+const Language = ({ currentLanguage }) => {
+
+const handleChangeLanguage = () => {
+    if (currentLanguage === 'hu') {
+        localStorage.setItem('language', 'srb')
+    } else {
+        localStorage.setItem('language', 'hu')
+    }
+    window.location.reload()
+}
+
+  return (
+    <div onClick={handleChangeLanguage}>
+        <LanguageWrapper> 
+            <FlagWrapper>
+                <img src={currentLanguage !== 'hu' ? FlagHu : FlagSrb} width={24} alt="Flag" />
+            </FlagWrapper>
+            <Text>
+            {currentLanguage !== 'hu' ? 'Magyar' : 'Srpski'}
+            </Text>
+        </LanguageWrapper>
+    </div>
+    
+  )
+}
+
+export default Language
+
